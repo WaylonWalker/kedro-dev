a13dfc6be7c5bd6cb9e636115c1e60a3970eeb20
WaylonWalker <walker_waylon_s@cat.com>
2020-06-23 19:39:31 -0500

    add stack overflow and devto components

---
+++ src/components/DevTo.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import FadeIn from 'src/components/FadeIn'
+import axios from 'axios'
+
+const Styles = styled.div`
+/* max-width:800px; */
+margin: 1rem;
+.posts {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+}
+
+.post {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    background: rgba(255, 255, 255, .1);
+    margin: 1rem;
+    padding: .5rem;
+    min-height: 100px;
+    max-width: 800px;
+    box-shadow: 0px 0px 19px -5px rgba(255,255,255,0.1);
+}
+
+.post-content {
+    width: 300px;
+}
+
+h3 {
+    font-weight: 400;
+    justify-self: flex-start;
+    align-self: center;
+}
+
+h2 .count {
+    transform: rotate(-8deg);
+    display: inline-block;
+}
+
+ .post-meta {
+     display: flex;
+     flex-direction: column;
+     /* flex-wrap: wrap; */
+     justify-content: center;
+     align-items: center;
+ }
+
+ .post-meta > div {
+     margin: .2rem;
+ }
+
+.post a {
+    color: white;
+    text-decoration: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.is-answered {
+    color: #73C991;
+}
+
+.post-date {
+    color: #bdc8c9;
+}
+
+.post-owner {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* max-width: 50px; */
+}
+
+.post-owner-image{
+    max-width: 50px;
+    margin: 0.1rem;
+    padding: 0;
+    border-radius: 68% 32% 19% 81% / 29% 27% 73% 71% ;
+}
+.post-owner span {
+    text-align: center;
+}
+`
+
+const htmlDecode = innerHTML => Object.assign(document.createElement('textarea'), { innerHTML }).value;
+
+const localDate = (d) => {
+    let date = new Date(d)
+    return date.toLocaleDateString()
+}
+
+const Post = ({ post }) => {
+    return (
+        <div className="post">
+            <div className="post-content">
+                <h3>
+                    <a href={post.url}>
+                        {htmlDecode(post.title)}
+                    </a>
+                </h3>
+                <p>
+                    {post.description}
+                </p>
+
+            </div>
+            <div className="post-meta">
+                <div className="post-owner">
+                    <a href={post.user.website_url}>
+                        <img src={post.user.profile_image} title={post.user.name} alt={`${post.user.display_name} profile from Stack Overflow`} className='post-owner-image' />
+                        <div className='post-owner-name'>
+                            {post.user.name}
+                        </div>
+                    </a>
+                </div>
+                <div className="post-views"><span role='img'>♥</span>{post.public_reactions_count}</div>
+                <div className="post-date">{localDate(post.published_timestamp)}</div>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+const DevTo = () => {
+    const [data, setData] = useState([])
+
+    useEffect(async () => {
+        const result = await axios(
+            'https://dev.to/api/articles?tag=kedro&per_page=5&page=1'
+
+        )
+        setData(result.data)
+    }, [])
+
+    return (
+        <Styles>
+            <FadeIn delay={100}>
+                <div className="posts">
+                    <h2><span className='count'>5</span> most Recent Articles on <a href='https://dev.to/t/kedro' title='kedro tag on DEV.to'>Dev.to</a></h2>
+                    {data.map(post => <Post post={post} />)}
+                </div>
+            </FadeIn>
+        </Styles>
+    )
+}
+
+export default DevTo
---
+++ src/components/StackOverflow.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import FadeIn from 'src/components/FadeIn'
+import axios from 'axios'
+
+const Styles = styled.div`
+margin: 1rem;
+.posts {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+}
+
+.post {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    background: rgba(255, 255, 255, .1);
+    margin: 1rem;
+    padding: .5rem;
+    min-height: 100px;
+    max-width: 500px;
+    box-shadow: 0px 0px 19px -5px rgba(255,255,255,0.1);
+}
+
+h3 {
+    width: 300px;
+    font-weight: 400;
+    justify-self: flex-start;
+    align-self: center;
+}
+
+h2 .count {
+    transform: rotate(-8deg);
+    display: inline-block;
+}
+
+ .post-meta {
+     display: flex;
+     /* flex-wrap: wrap; */
+     justify-content: center;
+     align-items: center;
+ }
+
+ .post-meta > div {
+     margin: .2rem;
+ }
+
+.post a {
+    color: white;
+    text-decoration: none;
+}
+
+.is-answered {
+    color: #73C991;
+}
+
+.post-date {
+    color: #bdc8c9;
+}
+.post-owner {
+    max-width: 50px;
+}
+
+.post-owner-image{
+    margin: 0;
+    padding: 0;
+    border-radius: 68% 32% 19% 81% / 29% 27% 73% 71% ;
+}
+.post-owner span {
+    text-align: center;
+}
+`
+
+const htmlDecode = innerHTML => Object.assign(document.createElement('textarea'), { innerHTML }).value;
+
+const localDate = (d) => {
+    let date = new Date(0)
+    date.setUTCSeconds(d)
+    return date.toLocaleDateString()
+}
+
+const Post = ({ post }) => {
+    return (
+        <div className="post">
+            <h3>
+                <a href={post.link}>
+                    {htmlDecode(post.title)}
+                </a>
+            </h3>
+            <div className="post-meta">
+                <div className="meta-left">
+                    <div className="is-answered">{post.is_answered === true ? '✔answered' : ''}</div>
+                    <div className="post-views"><span role='img'>👀</span>{post.view_count}</div>
+                    <div className="post-date">{localDate(post.creation_date)}</div>
+
+                </div>
+                <div className="post-owner">
+                    <a href={post.owner.link}>
+                        <img src={post.owner.profile_image} title={post.owner.display_name} alt={`${post.owner.display_name} profile from Stack Overflow`} className='post-owner-image' />
+                        <span>
+                            {post.owner.display_name}
+                        </span>
+                    </a>
+                </div>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+const StackOverflow = () => {
+    const [data, setData] = useState([])
+
+    useEffect(async () => {
+        const result = await axios(
+            'https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&tagged=kedro&pagesize=5'
+        )
+        setData(result.data.items)
+    }, [])
+
+    return (
+        <Styles>
+            <FadeIn delay={100}>
+                <div className="posts">
+                    <h2><span className='count'>5</span> most Recent Questions from <a href='https://stackoverflow.com/questions/tagged/kedro' title='kedro tag on stack overflow'>Stack Overflow</a></h2>
+                    {data.map(post => <Post post={post} />)}
+                </div>
+            </FadeIn>
+        </Styles>
+    )
+}
+
+export default StackOverflow
